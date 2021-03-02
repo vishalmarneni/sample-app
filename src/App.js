fc4f4adbcdbf8fd655aca0ee79f81a2ca7ffe83e
vishal marneni <vishal.marneni@integritymarketing.com>
2021-03-02 17:01:50 -0600

    show user data in table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import UsersTable from './components/UsersTable'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Deploy app to netlify.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <UsersTable />
     </div>
   );
 }
---
+++ src/components/UsersTable/index.js
@@ -0,0 +1,72 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import { useTable } from 'react-table';
+
+import useUsers from "./useUsers";
+import Loader from "../Loader";
+import React from 'react';
+
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+const UserTable = () => {
+  const {
+    isLoading,
+    usersData,
+    columns,
+    error
+  } = useUsers();
+
+  const classes = useStyles();
+
+  const { getTableProps, headerGroups, rows, prepareRow } = useTable({
+    columns,
+    data: usersData,
+  })
+  console.log(rows)
+  return isLoading
+    ? <Loader />
+    : (<TableContainer component={Paper}>
+      <Table className={classes.table} size="small" aria-label="a dense table" {...getTableProps()}>
+        <TableHead>
+        {headerGroups.map(headerGroup => (
+          <TableRow {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <TableCell {...column.getHeaderProps()}>{column.render('Header')}</TableCell>
+            ))}
+          </TableRow>
+        ))}
+        </TableHead>
+        <TableBody>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          console.log(row)
+          return (
+            <TableRow {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return (
+                  <TableCell {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </TableCell>
+                )
+              })}
+            </TableRow>
+          )
+        })}
+        </TableBody>
+      </Table>
+    </TableContainer>);
+};
+
+export default UserTable;
