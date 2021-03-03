c52fe67787250c5960e3dd5e92d7109a4e984bce
vishal marneni <vishal.marneni@integritymarketing.com>
2021-03-02 18:06:19 -0600

    Add Environment Variables to Netlify Sites

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 import './App.css';
 import UsersTable from './components/UsersTable'
 import CssBaseline from '@material-ui/core/CssBaseline'
 
+const environment = process.env.NODE_ENV;
+const sampleTest = process.env.REACT_APP_ENV_TEST;
+
 function App() {
   return (
     <div className="App">
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+          {environment} - {sampleTest}
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <CssBaseline />
       <UsersTable />
     </div>
